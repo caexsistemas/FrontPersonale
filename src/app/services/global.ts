9ce4c71b49cfddde9ce2fa5c26fba4b6ce8e02b4
VictorSaavedraCaex <92398760+VictorSaavedraCaex@users.noreply.github.com>
2021-11-10 10:56:31 -0500

    2020-11-10 Descargue

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export const global = {
     // url: 'http://192.168.0.54/gesPersonale/index.php/',
-    url: 'http://127.0.0.1:8000',
+    url: 'http://127.0.0.1:8000/api',
 }
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  url: "http://127.0.0.1:8080/api"
 };
