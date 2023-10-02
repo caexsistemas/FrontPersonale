54c607f86880d984816daf7b22abd08fedd31734
Camilo <juanescobb91@gmail.com>
2023-10-02 11:59:04 -0500

    02-10-2023

--- src/app/views/management/management.component.ts
+++ src/app/views/management/management.component.ts
@@ -164,7 +164,8 @@ export class ManagementComponent implements OnInit {
             !(
               this.cuser.role == 1 ||
               this.cuser.role == 5 ||
-              this.cuser.role == 20
+              this.cuser.role == 20 ||
+              this.cuser.role == 34
             )
           ) {
             this.checkCreate = false;
