ba767b3c3599c4d28b348f5fe2b0ae395db8b025
Camilo <juanescobb91@gmail.com>
2023-11-01 08:15:20 -0500

    descargable contact

--- src/app/views/callcenter/callcenter.module.ts
+++ src/app/views/callcenter/callcenter.module.ts
@@ -1,51 +1,52 @@
 /**
-    * @description      : 
-    * @author           : Victor Duran
-    * @group            : 
-    * @created          : 24/06/2021 - 15:20:26
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 24/06/2021
-    * - Author          : Victor Duran
-    * - Modification    : 
-**/
+ * @description      :
+ * @author           : Victor Duran
+ * @group            :
+ * @created          : 24/06/2021 - 15:20:26
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 24/06/2021
+ * - Author          : Victor Duran
+ * - Modification    :
+ **/
 
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
-
-import { TextMaskModule } from 'angular2-text-mask';
-import { SelectModule } from 'ng-select';
+import { TextMaskModule } from "angular2-text-mask";
+import { SelectModule } from "ng-select";
 
 // Components Routing
-import { CallcenterRoutingModule } from './callcenter-routing.module';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { CallcenterRoutingModule } from "./callcenter-routing.module";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TabsModule } from "ngx-bootstrap/tabs";
 
 // Modal Component
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { DatafilterModule } from '../../Tools/pipe/datafilter.module';
-import { MatInputModule }  from '@angular/material/input';
-import { NgWizardModule, NgWizardConfig, THEME } from 'ng-wizard';
-import { RqcalidadComponent } from './rqcalidad/rqcalidad.component';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { UnicalldataComponent } from './unicalldata/unicalldata.component';
-import { CustomerComponent } from './customer/customer.component';
+import { ModalModule } from "ngx-bootstrap/modal";
+import { DatafilterModule } from "../../Tools/pipe/datafilter.module";
+import { MatInputModule } from "@angular/material/input";
+import { NgWizardModule, NgWizardConfig, THEME } from "ng-wizard";
+import { RqcalidadComponent } from "./rqcalidad/rqcalidad.component";
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import { FeedbackComponent } from "./feedback/feedback.component";
+import { UnicalldataComponent } from "./unicalldata/unicalldata.component";
+import { CustomerComponent } from "./customer/customer.component";
+import { DwlcontactComponent } from "./dwlcontact/dwlcontact.component";
+import { MatSelectModule } from "@angular/material/select";
+import { MatDialogModule } from "@angular/material/dialog";
 //import { ReportsRqcalidadComponent } from "../../dialogs/reports/rqcalidad/ReportsRqcalidadComponent";
 
-
 const ngWizardConfig: NgWizardConfig = {
-  theme: THEME.arrows
+  theme: THEME.arrows,
 };
 
 @NgModule({
@@ -70,13 +71,15 @@ const ngWizardConfig: NgWizardConfig = {
     BsDropdownModule.forRoot(),
     NgWizardModule.forRoot(ngWizardConfig),
     MatBottomSheetModule,
-    
-
+    MatSelectModule,
+    MatDialogModule,
   ],
   declarations: [
-  RqcalidadComponent,
-  FeedbackComponent,
-  UnicalldataComponent,
-  CustomerComponent]
+    RqcalidadComponent,
+    FeedbackComponent,
+    UnicalldataComponent,
+    CustomerComponent,
+    DwlcontactComponent,
+  ],
 })
-export class CallcenterModule { }
+export class CallcenterModule {}
